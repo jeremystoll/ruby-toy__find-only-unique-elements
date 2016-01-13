3b0d7469e7e71e7e932feea01b6c28b5297a7086
Jeremy Stoll <j8r8my@gmail.com>
2016-01-13 02:47:33 +0000

    HalfwayThere

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -1,6 +1,19 @@
 # This method takes an array of elements (these might be strings, integers, floats, or a
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
-def find_unique_elements(arr)
+
+# input an array 
+
+# for each element:
+# look for an equal element in the array
+# if found, then stop
+# else add to (return_array)
+#end
+# puts (return_array)
+
+
+
+def find_unique_elements (arr)
+    
   
 end
